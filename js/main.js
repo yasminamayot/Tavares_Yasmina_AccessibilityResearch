908a4c0687e10126f96d65588851ff66d0f439f5
yasminamayot <yasminamayot@gmail.com>
2020-04-03 21:12:37 -0400

    added final js components for accessibility

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 function revealText() {
     document.querySelector('.showLyrics').style.display='block';
 }
+
+// Increase/descrease font size
+$('#increasetext').click(function() {
+    curSize = parseInt($('#content').css('font-size')) + 2;
+    if (curSize <= 32)
+        $('#content').css('font-size', curSize);
+});
+
+$('#resettext').click(function() {
+    if (curSize != 18)
+        $('#content').css('font-size', 18);
+});
+
+$('#decreasetext').click(function() {
+    curSize = parseInt($('#content').css('font-size')) - 2;
+    if (curSize >= 14)
+        $('#content').css('font-size', curSize);
+});
+
+
+document.querySelector('.showLyrics').style.display='none';
